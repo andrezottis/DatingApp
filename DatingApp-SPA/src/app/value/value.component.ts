141887e0cb82bbd15ea105a0c4659b12a2b9b345
André Zottis <zottis.andre@outlook.com>
2020-01-07 17:42:14 -0300

    Add values component to main page and got values from api. (not visible yet).

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -12,10 +12,11 @@ export class ValueComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
 
   getValues(){
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+    this.http.get('http://localhost:5000/api/values/').subscribe(response => {
       this.values = response;
     }, error => {
       console.log(error);
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -29,6 +29,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,11 +44,14 @@ namespace DatingApp.API
 
             app.UseRouting();
 
+            app.UseCors( x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                //endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
