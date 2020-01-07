02387d56340b1ee617d19c23e3f09c7e38a33884
André Zottis <zottis.andre@outlook.com>
2020-01-07 14:50:10 -0300

    Add request from DB and changed to Async API.

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -2,26 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
-     [Route("api/[controller]")]
-     [ApiController]
-     public class ValuesController : ControllerBase
-     {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
         //GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] {"value 1", "value 2"};
+            var values =await _context.Values.ToListAsync();
+            return Ok(values);
         }
 
         //GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value =await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
         }
 
         //POST api/values
@@ -33,16 +43,16 @@ namespace DatingApp.API.Controllers
 
         //PUT api/values/5
         [HttpPut("{id}")]
-        public void Put (int id, [FromBody] string value)
+        public void Put(int id, [FromBody] string value)
         {
 
         }
 
         //DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete (int id)
+        public void Delete(int id)
         {
 
         }
-     }
+    }
 }
