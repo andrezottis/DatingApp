ccbef4ee5122d5ae7a6daf3979865a7b95ebdd90
Andre Zottis <Andre.Zottis@andritz.com>
2020-01-27 10:10:27 -0300

    Add new Nuget packages and  Insert Data Annotation

--- DatingApp.API/DTOs/UserForRegisterDTO.cs
+++ DatingApp.API/DTOs/UserForRegisterDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string Username {get ;set;}
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must enter password between 4 and 8 char.")] 
         public string Password { get; set; }
         
     }
